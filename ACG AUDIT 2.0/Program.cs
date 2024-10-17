de52435838f32eb1773e76027e2594633d94a9a9
Gustavo Fernandes <gustavo.freitas.afel@gmail.com>
2024-10-17 13:51:44 -0300

    Ignorando agora com sucesso (reorganizando)

--- ACG AUDIT 2.0/Program.cs
+++ ACG AUDIT 2.0/Program.cs
@@ -8,24 +8,6 @@ namespace RegistryPolReader
     {
         static void Main(string[] args)
         {
-            // Caminho relativo do arquivo Registry.pol
-            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "GroupPolicy", "User", "Registry.pol");
-            Console.WriteLine($"\nArquivo Pesquisado\nFile path: {filePath}");
-            if (!File.Exists(filePath))
-            {
-                Console.WriteLine("O arquivo Registry.pol não foi encontrado.");
-                return;
-            }
-
-            // Criar uma instância de PolicyExtractor e executar a extração
-            PolicyExtractor extractor = new PolicyExtractor();
-            extractor.ExtractPolicyValues(filePath);
-
-            // Verificar o status do BitLocker para todas as unidades
-            BitLockerInfo.CheckBitLockerStatusForAllDisks();
-
-            // Coletar e exibir informações sobre softwares instalados
-            SoftwareCollector.CollectAndDisplayInstalledSoftwares();
 
             GetDeviceId.GetUUID();
 
@@ -38,6 +20,26 @@ namespace RegistryPolReader
             Console.WriteLine($"Armazenamento: {systemInfo.GetStorageInfo()}");
             Console.WriteLine($"Rede: {systemInfo.GetNetworkInfo()}");
             Console.WriteLine();
+
+             // SoftwareCollector.CollectAndDisplayInstalledSoftwares();
+            // Criar uma instância de PolicyExtractor e executar a extração
+
+             // Caminho relativo do arquivo Registry.pol
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "GroupPolicy", "User", "Registry.pol");
+            Console.WriteLine($"\nArquivo Pesquisado\nFile path: {filePath}");
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("O arquivo Registry.pol não foi encontrado.");
+                return;
+            }
+
+            PolicyExtractor extractor = new PolicyExtractor();
+            extractor.ExtractPolicyValues(filePath);
+
+            // Verificar o status do BitLocker para todas as unidades
+            BitLockerInfo.CheckBitLockerStatusForAllDisks();
+
+            // Coletar e exibir informações sobre softwares instalados
 
             Console.ReadLine(); // Stop console
         }
