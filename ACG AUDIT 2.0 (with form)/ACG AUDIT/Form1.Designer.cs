57a9a1e2c2d8f7d47a53d1f5eadaf60fa0c8663e
Gustavo Fernandes <gustavo.freitas.afel@gmail.com>
2024-10-28 08:34:36 -0300

    create a form

--- ACG AUDIT 2.0 (with form)/ACG AUDIT/Form1.Designer.cs
+++ ACG AUDIT 2.0 (with form)/ACG AUDIT/Form1.Designer.cs
@@ -28,25 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(588, 258);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(56, 50);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Teste";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.LinkColor = System.Drawing.Color.Black;
-            this.linkLabel1.Location = new System.Drawing.Point(342, 41);
-            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Location = new System.Drawing.Point(337, 21);
+            this.linkLabel1.Name = "Tela_inicial";
             this.linkLabel1.Size = new System.Drawing.Size(65, 13);
             this.linkLabel1.TabIndex = 1;
             this.linkLabel1.TabStop = true;
@@ -58,9 +48,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "tela_inicial";
+            this.Tag = "";
+            this.Text = "ACG AUDIT 2.0";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -68,8 +58,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
