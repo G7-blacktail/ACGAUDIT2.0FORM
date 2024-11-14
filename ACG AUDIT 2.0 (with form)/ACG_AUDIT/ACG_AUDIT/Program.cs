619c6ab5c9e284dca36e78a1ac05f54ecbdc87c1
Gustavo Fernandes <gustavo.freitas.afel@gmail.com>
2024-11-14 13:37:34 -0300

    Executando com o agendador

--- ACG AUDIT 2.0 (with form)/ACG_AUDIT/ACG_AUDIT/Program.cs
+++ ACG AUDIT 2.0 (with form)/ACG_AUDIT/ACG_AUDIT/Program.cs
@@ -1,52 +1,3 @@
-//using ACG_AUDIT.Services;
-
-//namespace ACG_AUDIT
-//{
-//    internal static class Program
-//    {
-//        /// <summary>
-//        ///  The main entry point for the application.
-//        /// </summary>
-//        [STAThread]
-//        static void Main()
-//        {
-//            Application.EnableVisualStyles();
-//            Application.SetCompatibleTextRenderingDefault(false);
-
-//            // Criar e mostrar a tela de carregamento
-//            TelaInicial loadingForm = new TelaInicial();
-//            loadingForm.Show();
-
-//            // Executar o trabalho em segundo plano
-//            Task.Run(() =>
-//            {
-//                // Simulando várias etapas de um processo
-//                for (int i = 0; i < 5; i++)
-//                {
-//                    loadingForm.Invoke((MethodInvoker)delegate
-//                    {
-//                        loadingForm.UpdateStatus($"Executando etapa {i + 1}...");
-//                    });
-
-//                    // Simula trabalho em cada etapa
-//                    Thread.Sleep(2000); // Simula 2 segundos de trabalho
-//                }
-
-//                // Fechar a tela de carregamento no thread da interface do usuário
-//                loadingForm.Invoke((MethodInvoker)delegate
-//                {
-//                    loadingForm.Close();
-//                });
-//            });
-
-//            // Mantém a aplicação em execução
-//            Application.Run();
-//        }
-//    }
-//}
-
-
-
 using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -96,7 +47,10 @@ namespace ACG_AUDIT
             });
 
             // Mantém a aplicação em execução
-            Application.Run();
+            Application.Run(loadingForm); // Mantenha a aplicação em execução até que o formulário de carregamento seja fechado
+
+            // Finaliza a aplicação após o fechamento do formulário
+            Application.Exit();
         }
     }
 }
